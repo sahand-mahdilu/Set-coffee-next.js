627ae84fed6956bfc1206054e3b076dc25e04a87
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-28 13:57:21 +0330

    feat : slider configs completed

--- src/Components/templates/Banner.tsx
+++ src/Components/templates/Banner.tsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 
-import { Navigation } from 'swiper/modules';
+import { Navigation,Autoplay } from 'swiper/modules';
 
 
 export default function Banner() {
@@ -12,10 +12,10 @@ export default function Banner() {
   return(
 
     <>
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      <SwiperSlide><img src="" alt="" /></SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
+    <Swiper loop={true} autoplay={{delay:2000}} rewind={true} navigation={true} modules={[Navigation,Autoplay]} className="mySwiper">
+      <SwiperSlide><img src="/images/slide-1.jpg" alt="slide-1" /></SwiperSlide>
+      <SwiperSlide><img src="/images/slide-2.jpg" alt="slide-2" /></SwiperSlide>
+      <SwiperSlide><img src="/images/slide-3.jpg" alt="slide-3" /></SwiperSlide>
      
   
     </Swiper>
