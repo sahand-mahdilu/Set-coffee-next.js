f4988f01a1fea408cb6a2c9a40911c8ed59dea6c
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-28 20:58:46 +0330

    feat : html template content added

--- src/Components/modules/product/Product.tsx
+++ src/Components/modules/product/Product.tsx
@@ -1,7 +1,43 @@
+import Link from 'next/link'
 import React from 'react'
+import { CiHeart, CiSearch } from 'react-icons/ci'
+import { FaRegStar, FaStar } from 'react-icons/fa'
 
 export default function Cart() {
   return (
-    <div>Product</div>
+    <div >
+      <div >
+        <img
+          src="https://set-coffee.com/wp-content/uploads/2021/10/041-430x430.png"
+          alt=""
+        />
+        <div >
+          <Link href="/">
+            <CiSearch />
+            <p >مشاهده سریع</p>
+          </Link>
+          <div>
+            <CiHeart />
+            <p >افزودن به علاقه مندی ها </p>
+          </div>
+        </div>
+        <button>افزودن به سبد خرید</button>
+      </div>
+
+      <div >
+        <Link href={"/"}>
+          کپسول قهوه SETpresso سازگار با دستگاه نسپرسو ( RED ) 10 عددی LIMITED
+          EDITION
+        </Link>
+        <div>
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaRegStar />
+          <FaRegStar />
+        </div>
+        <span>825,000 تومان</span>
+      </div>
+    </div>
   )
 }
