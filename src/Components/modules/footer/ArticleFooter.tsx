2672cc511040449d2cf55eee3ab06f0687e0661f
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-29 12:59:24 +0330

    html template added

--- src/Components/modules/footer/ArticleFooter.tsx
+++ src/Components/modules/footer/ArticleFooter.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
-export default function ArticleFooter() {
+export default function ArticleFooter({ title, img, comments, date, href }) {
   return (
-    <div>ArticleFooter</div>
+    <Link href={href} >
+      <img width={75} height={65} src={img} alt="" />
+      <div>
+        <p >{title}</p>
+        <div>
+          <p>{comments}</p>
+          <p dir="rtl">{date}</p>
+        </div>
+      </div>
+    </Link>
   )
 }
