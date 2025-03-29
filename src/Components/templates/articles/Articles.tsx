dbf4e523c9c2b954d2d719fc5b9016712527086a
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-29 11:29:35 +0330

    feat : articles component html template added

--- src/Components/templates/articles/Articles.tsx
+++ src/Components/templates/articles/Articles.tsx
@@ -1,7 +1,31 @@
+"use client"
 import React from 'react'
+import styles from "./articles.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
 
 export default function Articles() {
   return (
-    <div>Articles</div>
+    <div >
+      <p >مقالات ما</p>
+      <span >دانستنی های جذاب دنیای قهوه</span>
+      <main>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          dir="rtl"
+          autoplay={{ delay: 1500, disableOnInteraction: false }}
+          //   rewind={true}
+          loop={true}
+          navigation={true}
+          modules={[Navigation, Autoplay]}
+          className="mySwiper articles_slider"
+        >
+       
+        </Swiper>
+      </main>
+    </div>
   )
 }
