1876b540b85c5391425f4d202ec1884cd7ba7486
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-29 12:39:10 +0330

    feat : final styles added to article component

--- src/Components/templates/articles/Article.tsx
+++ src/Components/templates/articles/Article.tsx
@@ -1,41 +1,56 @@
-import Link from 'next/link'
-import React from 'react'
-import { FaFacebookF, FaLinkedinIn, FaPinterest, FaTelegram, FaTwitter } from 'react-icons/fa'
-import { IoShareSocialOutline } from 'react-icons/io5'
-import { MdOutlineSms } from 'react-icons/md'
+import Link from "next/link";
+import React from "react";
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaPinterest,
+  FaTelegram,
+  FaTwitter,
+} from "react-icons/fa";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { MdOutlineSms } from "react-icons/md";
 
 export default function Article() {
   return (
-    <div >
-      <Link  href={"/article/123"}>
+    <div className="relative ">
+      <Link className="relative" href={"/article/123"}>
         <img
           src="https://set-coffee.com/wp-content/uploads/elementor/thumbs/-%D9%82%D9%87%D9%88%D9%87-%D8%A8%D8%A7-%D8%B4%DB%8C%D8%B1-qi8xuncj4ordgstrl43mbg5jfj1ezzamf6v9rnitn0.jpg"
           alt=""
         />
       </Link>
-      <div >
+      <div className="flex flex-col bg-white text-lg font-bold absolute top-4 right-4 rounded-md p-2 justify-center text-center">
         <span>24</span>
         <span>بهمن</span>
       </div>
-      <div >
-        <span >قهوه</span>
-        <Link href={"/article/123"} >
+      <div>
+        <span className="bg-black p-2 text-center absolute text-white bottom-[60%] right-1/2 transform translate-x-1/2 translate-y-1/2">
+          قهوه
+        </span>
+        <Link
+          className="absolute top-[45%] left-1/2  text-2xl text-white font-bold transform -translate-x-[60%] "
+          href={"/article/123"}
+        >
           مصرف قهوه با شیر برای کاهش التهاب
         </Link>
-        <div>
+        <div className=" absolute  bottom-4 flex items-center gap-4 text-gray-100 left-1/2 transform -translate-x-1/2 ">
           <p>نویسنده</p>
           <img
+            className="rounded-full size-9"
             src="https://secure.gravatar.com/avatar/665a1a4dc7cc052eaa938253ef413a78?s=32&d=mm&r=g"
             alt=""
           />
           <p>Mohebi</p>
-          <div>
-            <MdOutlineSms />
-            <span>0</span>
+          <div className="relative ">
+            <MdOutlineSms className="cursor-pointer"/>
+            <span className="absolute text-[9px] flex justify-center items-center top-[-10px] right-[-10px] bg-black text-white p-1 font-bold size-4  text-center rounded-full">
+              0
+            </span>
           </div>
-          <div >
-            <IoShareSocialOutline />
-            <div >
+          <div className="group relative">
+            
+            <IoShareSocialOutline className="text-blue-500 group cursor-pointer " />
+            <div className="absolute  right-[-20px] hidden gap-[6px] bg-black text-white p-1 group-hover:flex   ">
               <Link href={"/"}>
                 <FaTelegram />
               </Link>
@@ -56,5 +71,5 @@ export default function Article() {
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/Components/templates/articles/Articles.tsx
+++ src/Components/templates/articles/Articles.tsx
@@ -1,50 +1,39 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import styles from "./articles.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Autoplay, Navigation } from "swiper/modules";
+import Article from "./Article";
 
 export default function Articles() {
   return (
-    <div className=' container w-[90%]'>
-      <p className='text-3xl font-bold'>مقالات ما</p>
-      <span className='text-gray-400'>دانستنی های جذاب دنیای قهوه</span>
-      <main className='mt-10'>
+    <div className=" container w-[90%]">
+      <p className="text-3xl font-bold">مقالات ما</p>
+      <span className="text-gray-400">دانستنی های جذاب دنیای قهوه</span>
+      <main className="mt-10">
         <Swiper
           slidesPerView={3}
           spaceBetween={30}
           dir="rtl"
-          autoplay={{ delay: 1500, disableOnInteraction: false }}
+        //   autoplay={{ delay: 1500, disableOnInteraction: false }}
           //   rewind={true}
           loop={true}
           navigation={true}
           modules={[Navigation, Autoplay]}
-          className="mySwiper articles_slider"
         >
-
-<SwiperSlide>
-sdfg          </SwiperSlide>
-          <SwiperSlide>
-sdfsd          </SwiperSlide>
-          <SwiperSlide>
-sfdsd          </SwiperSlide>
-          <SwiperSlide>
-sdf          </SwiperSlide>
-          <SwiperSlide>
-sdf          </SwiperSlide>
-          <SwiperSlide>
-dsf          </SwiperSlide>
-          <SwiperSlide>
-dsaf          </SwiperSlide>
-          <SwiperSlide>
-sadf          </SwiperSlide>
-          <SwiperSlide>
-dsaf          </SwiperSlide>
-       
+          <SwiperSlide ><Article/> </SwiperSlide>
+          <SwiperSlide>sdfsd </SwiperSlide>
+          <SwiperSlide>sfdsd </SwiperSlide>
+          <SwiperSlide>sdf </SwiperSlide>
+          <SwiperSlide>sdf </SwiperSlide>
+          <SwiperSlide>dsf </SwiperSlide>
+          <SwiperSlide>dsaf </SwiperSlide>
+          <SwiperSlide>sadf </SwiperSlide>
+          <SwiperSlide>dsaf </SwiperSlide>
         </Swiper>
       </main>
     </div>
-  )
+  );
 }
