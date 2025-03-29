0245c40cfe9ef4de04df76fc58c31c518595a856
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-29 21:47:49 +0330

    feat : scroll to top added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AosInit from "@/utils/AosInit";
+import ScrollToTop from "@/utils/scrollTotop/ScrollTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <AosInit/>
         {children}
+        <ScrollToTop/>
 
       </body>
     </html>
