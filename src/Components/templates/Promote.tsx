85e6b1bd8f287309124af22985d2a3464f155dfd
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-29 10:58:01 +0330

    feat : promote html template added

--- src/Components/templates/Promote.tsx
+++ src/Components/templates/Promote.tsx
@@ -1,7 +1,50 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Promote() {
   return (
-    <div>Promote</div>
+    <div className='bg-gray-200 flex flex-col items-center m-16'>
+      <div className='max-w-[1222px] flex flex-col justify-center' data-aos="fade-up-right">
+        <main className='flex justify-between'>
+          <section >
+            <span>خرید قهوه ، به سبک حرفه ای ها</span>
+            <p>زیبایی امروز رو با قهوه “ست” کنید</p>
+            <img data-aos="fade-left" src="/images/coffee-image-1.jpg" alt="" />
+          </section>
+          <section >
+            <img src="/images/clubset1.jpg" alt="" />
+            <div>
+              <span>باشگاه مشتریان ست</span>
+              <p>برای مشتریان وفادار قهوه ست</p>
+            </div>
+          </section>
+        </main>
+        <main >
+          <img width={660} height={530} src="/images/Home32.jpg" alt="" />
+          <section data-aos="fade-up" >
+            <img
+              
+              src="/images/coffee-svg-2.svg"
+              alt=""
+            />
+            <p >چرا قهوه ست</p>
+            <p>
+              برخورداری از تجربه و قدمت کافی و آگاهی از ذایقه مصرف کنندگان
+              راهنمای ما در برآورده ساختن نیاز مشتریان قهوه تخصصی (موج سوم) است
+              .تجربه ای به قدمت چهار نسل و ارتباط مستمر با مصرف کنندگان قهوه
+              ضامن این ویژگیها است.
+            </p>
+            <div>
+              <Link href="/about-us">
+                <button >بیشتر بخوانید</button>
+              </Link>
+              <Link href="/category">
+                <button>فروشگاه</button>
+              </Link>
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/Components/modules/navbar/Navbar'
 import Banner from '@/Components/templates/Banner'
 import Lastest from '@/Components/templates/Lastest'
+import Promote from '@/Components/templates/Promote'
 import React from 'react'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <Navbar/>
     <Banner/>
     <Lastest/>
+    <Promote/>
   </>
   )
 }
