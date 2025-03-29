459114271bb0b02409040ad2705746c3a7c5fc0a
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-29 14:49:45 +0330

    feat : responsive desing added to lastest component

--- src/Components/templates/Lastest.tsx
+++ src/Components/templates/Lastest.tsx
@@ -5,7 +5,7 @@ import Cart from "../modules/product/Cart";
 
 export default function Lastest() {
   return (
-    <div className="container w-[90%]">
+    <div className="container  max-lg:text-[14px] ">
       <section className="flex justify-between">
         <div className="flex flex-col">
           <p className="text-3xl font-semibold">آخرین محصولات</p>
@@ -15,7 +15,7 @@ export default function Lastest() {
           مشاهده همه <FaChevronLeft />{" "}
         </Link>
       </section>
-      <main data-aos="zoom-in" className="grid grid-cols-4 gap-5">
+      <main data-aos="zoom-in" className="grid grid-cols-4 gap-5 max-md:grid-cols-3 max-sm:grid-cols-1 max-sm:justify-center">
         <Cart />
         <Cart />
         <Cart />
