2a103daa6653ec7cadae3da421a058ae5df502f4
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-29 10:35:10 +0330

    feat : promote componenet created

--- src/Components/templates/Lastest.tsx
+++ src/Components/templates/Lastest.tsx
@@ -1,35 +1,32 @@
-import Link from 'next/link'
-import React from 'react'
-import { FaChevronLeft } from 'react-icons/fa'
-import Cart from '../modules/product/Cart'
+import Link from "next/link";
+import React from "react";
+import { FaChevronLeft } from "react-icons/fa";
+import Cart from "../modules/product/Cart";
 
 export default function Lastest() {
   return (
-    <div className='container w-[90%]'>
-      <section  className='flex justify-between'>
-        <div className='flex flex-col'>
-          <p className='text-3xl font-semibold'>آخرین محصولات</p>
-          <span className='mt-2'>Latest products</span>
+    <div className="container w-[90%]">
+      <section className="flex justify-between">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold">آخرین محصولات</p>
+          <span className="mt-2">Latest products</span>
         </div>
-        <Link className='flex items-center'  href={"/category"}>
+        <Link className="flex items-center" href={"/category"}>
           مشاهده همه <FaChevronLeft />{" "}
         </Link>
       </section>
-      <main data-aos="zoom-in" className='grid grid-cols-4 gap-5' >
+      <main data-aos="zoom-in" className="grid grid-cols-4 gap-5">
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
 
-
-    
-        <Cart/>
-        <Cart/>
-        <Cart/>
-        <Cart/>
-        <Cart/>
-        <Cart/>
-        <Cart/>
-        <Cart/>
-
-    {/* products */}
+        {/* products */}
       </main>
     </div>
-  )
+  );
 }
