70351fd931a14ce3db02058a036c569d4abaef89
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-30 19:13:28 +0330

    user model completed

--- models/User.ts
+++ models/User.ts
@@ -1,30 +1,43 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document, Model } from "mongoose";
 
 
-const schema= new mongoose.Schema({
+interface IUser extends Document {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  role?: string; 
+  refreshToken?: string; 
+}
 
-    name:{
-        type:String,
-        required:true
-    },
-    username:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        default:"USER"
-    },
-  refreshToken: String,
-    
+const userSchema: Schema<IUser> = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "USER",
+  },
+  refreshToken: {
+    type: String,
+  },
+});
 
 
-})
+const UserModel: Model<IUser> =
+  mongoose.models.user || mongoose.model<IUser>("user", userSchema);
+
+export { UserModel };
--- src/app/api/auth/signUp/route.ts
+++ src/app/api/auth/signUp/route.ts
@@ -3,13 +3,21 @@ import connectedToDB from "../../../../../configs/db";
 
 export async function GET(req: Request): Promise<NextResponse> {
   try {
-   
     await connectedToDB();
 
+    const reqBody = await req.json();
+
+    const { name, username, email, password } = reqBody;
+
+    // validation
     
-    return NextResponse.json({ message: "success response 11:)11" }, { status: 201 });
+
+
+    return NextResponse.json(
+      { message: "success response 11:)11" },
+      { status: 201 }
+    );
   } catch (error) {
-   
     console.error("Error:", error);
     return NextResponse.json(
       { message: "An error occurred while connecting to the database" },
