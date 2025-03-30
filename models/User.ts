c8a64319003b652972f16a6f3c9d55d1dc4dd943
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-30 14:26:21 +0330

    feat : schema added

---
+++ models/User.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+const schema= new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:"USER"
+    },
+  refreshToken: String,
+    
+
+
+})
