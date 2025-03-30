126f6ec7da59d116019a7519c262c07e1e9faade
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-30 18:26:15 +0330

    feat : generateAccessToken added

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,25 +1,32 @@
 import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
-const hashPassword = async (password: string):Promise<string>  =>  {
+const hashPassword = async (password: string): Promise<string> => {
   const hashedPassword = hash(password, 12);
 
   return hashedPassword;
 };
 
+const verifyPassword = async (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  const isValid = await compare(password, hashedPassword);
 
-const verifyPassword= async(password:string,hashedPassword:string):Promise<boolean> =>{
+  return isValid;
+};
 
-    const isValid = await compare(password,hashedPassword)
+const generateAccessToken = (data: object) => {
+  const secretKey = process.env.AccessTokenSecretKey;
+  if (!secretKey) {
+    throw new Error(
+      "AccessTokenSecretKey is not defined in environment variables"
+    );
+  }
 
-    return isValid
+  const token = sign({ ...data }, secretKey, {
+    expiresIn: "60s",
+  });
 
-}
-
-
-const generateAccessToken = (data)=>{
-
-    const token = 
-}
-
-
-
+  return token
+};
