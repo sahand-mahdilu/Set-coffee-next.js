bed1a6cdd321604d805af51c0706e796a1e3ba3c
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-30 14:53:55 +0330

    feat: type added

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,13 +1,13 @@
 import { compare, hash } from "bcryptjs";
 
-const hashPassword = async (password: string) => {
+const hashPassword = async (password: string):Promise<string>  =>  {
   const hashedPassword = hash(password, 12);
 
   return hashedPassword;
 };
 
 
-const verifyPassword= async(password:string,hashedPassword:string) =>{
+const verifyPassword= async(password:string,hashedPassword:string):Promise<boolean> =>{
 
     const isValid = await compare(password,hashedPassword)
 
