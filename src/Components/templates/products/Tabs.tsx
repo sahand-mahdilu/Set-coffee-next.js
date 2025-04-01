d512a89e46ff8285ea4a9e92ac0371305b55b9d5
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-01 11:49:47 +0330

    feat : product dynamic route created

---
+++ src/Components/templates/products/Tabs.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import styles from "./tabs.module.css";
+import { useState } from "react";
+import Description from "./Description";
+import Comments from "./Comments";
+import MoreInfos from "./MoreInfos";
+const Tabs = () => {
+  const [tab, setTab] = useState("description");
+  return (
+    <div data-aos="fade-left" className={styles.tabs}>
+      <input
+        onClick={() => setTab("description")}
+        type="radio"
+        id="description"
+        name="tab-control"
+        checked={tab === "description"}
+
+      />
+      <input
+        onClick={() => setTab("moreInfoes")}
+        type="radio"
+        id="moreInfoes"
+        name="tab-control"
+        checked={tab === "moreInfoes"}
+      />
+      <input
+        onClick={() => setTab("comments")}
+        type="radio"
+        id="comments"
+        name="tab-control"
+        checked={tab === "comments"}
+      />
+      <ul>
+        <li title="Features">
+          <label htmlFor="description" role="button">
+            {" "}
+            توضیحات{" "}
+          </label>
+        </li>
+        <li title="Delivery Contents">
+          <label htmlFor="moreInfoes" role="button">
+            {" "}
+            اطلاعات بیشتر{" "}
+          </label>
+        </li>
+        <li title="Shipping">
+          <label htmlFor="comments" role="button">
+            {" "}
+            نظرات (7){" "}
+          </label>
+        </li>
+      </ul>
+
+      <div className={styles.contents}>
+        <section className={styles.tabs_content}>
+          <Description />
+        </section>
+        <section className={styles.tabs_content}>
+          <MoreInfos/>
+        </section>
+        <section className={styles.tabs_content}>
+          <Comments />
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,58 @@
+import styles from "@/styles/product.module.css";
+
+import { verifyToken } from "@/utils/auth";
+import { UserModel } from "../../../../models/User";
+import { cookies } from "next/headers";
+import Navbar from "@/Components/modules/navbar/Navbar";
+import Footer from "@/Components/modules/footer/Footer";
+import Gallery from "@/Components/templates/products/Gallery";
+import Detailes from "@/Components/templates/products/Detailes";
+import MoreProducts from "@/Components/templates/products/MoreProducts";
+import Tabs from "@/Components/templates/products/Tabs";
+
+const product = async () => {
+ 
+     //  getting token form cookies
+
+  const cookieInstant = cookies();
+
+  const token = (await cookieInstant).get("token")?.value; //string
+
+  let user = null;
+
+  //  checking if there is a token or not  logged in /notlogged in
+
+  if (token) {
+    // verifying token
+    const tokenPayload = verifyToken(token);
+
+    if (
+      typeof tokenPayload === "object" &&
+      tokenPayload !== null &&
+      "username" in tokenPayload
+    ) {
+      // finding user
+      user = await UserModel.findOne({ username: tokenPayload.username });
+    } else {
+      console.log("Token payload is not valid or does not contain 'username'.");
+    }
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <Navbar isLogin={user} />
+      <div data-aos="fade-up" className={styles.contents}>
+        <div className={styles.main}>
+          <Detailes />
+          <Gallery />
+        </div>
+        <Tabs />
+        <MoreProducts />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default product;
