d3414b45f101f9732268e172876766f0f7e55a9e
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-01 16:32:58 +0330

    validation added

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -12,7 +12,48 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     const reqBody = await req.json();
     const { username, body, email, score, productID } = reqBody;
 
-
+    // validation 
+    if (
+        !username ||
+        typeof username !== "string" ||
+        username.trim().length === 0
+      ) {
+        return NextResponse.json(
+          { message: "Invalid username: must be a non-empty string." },
+          { status: 400 }
+        );
+      }
+  
+      if (!body || typeof body !== "string" || body.trim().length === 0) {
+        return NextResponse.json(
+          { message: "Invalid body: must be a non-empty string." },
+          { status: 400 }
+        );
+      }
+  
+      if (!email || typeof email !== "string" || !email.includes("@")) {
+        return NextResponse.json(
+          { message: "Invalid email: must be a valid email address." },
+          { status: 400 }
+        );
+      }
+  
+      if (!score || typeof score !== "number" || score < 1 || score > 5) {
+        return NextResponse.json(
+          {
+            message: "Invalid score: must be a number between 1 and 5.",
+          },
+          { status: 400 }
+        );
+      }
+  
+      if (!productID || typeof productID !== "string") {
+        return NextResponse.json(
+          { message: "Invalid product ID: must be a valid string." },
+          { status: 400 }
+        );
+      }
+  
 
 
   
