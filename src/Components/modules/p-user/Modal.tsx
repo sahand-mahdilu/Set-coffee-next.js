819cd0172dd665c1520ba8934ddd755b2bbbcfea
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-04 11:32:23 +0330

    box component created

--- src/Components/modules/p-user/Modal.tsx
+++ src/Components/modules/p-user/Modal.tsx
@@ -2,7 +2,6 @@ import { IoClose } from "react-icons/io5";
 import styles from "./modal.module.css";
 import { ModalProps } from "@/app/types/types";
 
-
 const Modal: React.FC<ModalProps> = ({ hideModal, title, children }) => {
   return (
     <div className={styles.modal}>
