80921312ac57e93ad8bef432dbf27260a1e4e518
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-04 20:51:34 +0330

    feat : delete wishlist api added

---
+++ src/app/api/wishlist/[id]/route.tsx
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { authUser } from "@/utils/severHelpers";
+import connectedToDB from "../../../../../configs/db";
+import WishListModel from "../../../../../models/WishList";
+
+interface Params {
+  id: string;
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: Params }) {
+  try {
+    await connectedToDB();
+    const user = await authUser();
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "Please login first !!" },
+        { status: 401 }
+      );
+    }
+
+    const productID = params.id;
+    await WishListModel.findOneAndDelete({
+      user: user._id,
+      product: productID,
+    });
+
+    return NextResponse.json({ message: "Product removed successfully :))" });
+  } catch (err) {
+    return NextResponse.json(
+        { message: (err as Error).message || "An error occurred." },
+        { status: 500 }
+    
+    );
+  }
+}
