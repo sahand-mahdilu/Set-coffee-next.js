8f691f6741205e9b290fcfac03c50ff949facad7
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-04 23:11:49 +0330

    acount deteil page created

---
+++ src/app/p-user/account-details/page.tsx
@@ -0,0 +1,12 @@
+import UserPanelLayout from "@/Components/layouts/UserPanelLayout";
+import AccountDetails from "@/Components/templates/userAcountDetaile/AccountDetails";
+
+const page = () => {
+  return (
+    <UserPanelLayout>
+      <AccountDetails />
+    </UserPanelLayout>
+  );
+};
+
+export default page;
