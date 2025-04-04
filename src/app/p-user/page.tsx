1bae6519a68172ba806c6d3e957bc3889ca62899
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-04 11:43:09 +0330

    feat : content added to p-user page

--- src/app/p-user/page.tsx
+++ src/app/p-user/page.tsx
@@ -1,16 +1,27 @@
+
 import UserPanelLayout from "@/Components/layouts/UserPanelLayout";
-import React from "react";
+import Box from "@/Components/templates/p-user/Box";
+import Orders from "@/Components/templates/p-user/Orders";
+import Tickets from "@/Components/templates/p-user/Tickets";
+import styles from "@/styles/p-user/index.module.css";
 
-function Index() {
-
-    
-
-
+const page = () => {
   return (
     <UserPanelLayout>
-      <h1>Home Page</h1>
+      <main>
+        <section className={styles.boxes}>
+          <Box title="مجموع تیکت ها " value="20" />
+          <Box title="مجموع کامنت ها " value="0" />
+          <Box title="مجموع سفارشات" value="2" />
+          <Box title="مجموع علاقه مندی ها" value="10" />
+        </section>
+        <section className={styles.contents}>
+          <Tickets />
+          <Orders />
+        </section>
+      </main>
     </UserPanelLayout>
   );
-}
+};
 
-export default Index;
+export default page;
