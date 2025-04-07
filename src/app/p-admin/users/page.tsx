41040a5843d6a12e3c6a94769618eb40e2224d58
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-07 17:41:37 +0330

    feat : users page created

---
+++ src/app/p-admin/users/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "@/components/templates/p-admin/users/table.module.css";
+import connectedToDB from "../../../../configs/db";
+import { UserModel } from "../../../../models/User";
+import AdminPanelLayout from "@/Components/layouts/AdminPanelLayout";
+import DataTable from "@/Components/templates/p-admin/users/Table";
+
+
+const page = async () => {
+await connectedToDB()
+  const users = await UserModel.find({}).lean();
+
+  return (
+    <AdminPanelLayout>
+      <main>
+        {users.length === 0 ? (
+          <p className={styles.empty}>کاربری وجود ندارد</p>
+        ) : (
+          <DataTable
+            users={JSON.parse(JSON.stringify(users))}
+            title="لیست کاربران"
+          />
+        )}
+      </main>
+    </AdminPanelLayout>
+  );
+};
+
+export default page;
