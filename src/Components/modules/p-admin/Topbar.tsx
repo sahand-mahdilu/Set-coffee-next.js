a5e82edef797346aee79fe2dba96aa4e390feb65
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-07 12:03:55 +0330

    feat : responsive design added to admin top bar

--- src/Components/modules/p-admin/Topbar.tsx
+++ src/Components/modules/p-admin/Topbar.tsx
@@ -3,13 +3,13 @@ import { IoIosSearch, IoIosNotifications } from "react-icons/io";
 const Topbar = () => {
   return (
     <>
-      <div className={styles.topbar}>
-        <div className={styles.profile}>
-          <div>
-            <p>شاهین مشکل گشا</p>
-            <span>ادمین</span>
-          </div>
+      <div className={`${styles.topbar} py-2 px-5 max-sm:pl-[6px] fixed top-0 left-0 right-0 z-20`}>
+        <div className={`${styles.profile} flex items-center gap-[10px] flex-wrap` }>
           <img src="/images/shahin.jpg" alt="" />
+          <div>
+            <p className="max-sm:text-[11px]">شاهین مشکل گشا</p>
+            <span className="text-[14px] max-sm:text-[9px]">ادمین</span>
+          </div>
         </div>
         <section>
           <div className={styles.searchBox}>
