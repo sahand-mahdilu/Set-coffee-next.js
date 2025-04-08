6630c2f59024df0bdc340ea1e04d5e466c708b6a
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-08 13:48:16 +0330

    feat : cart page created

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,34 @@
+
+import Footer from "@/Components/modules/footer/Footer";
+import Navbar from "@/Components/modules/navbar/Navbar";
+import Stepper from "@/Components/modules/stepper/Stepper";
+import Table from "@/Components/templates/cart/Table";
+import styles from "@/styles/cart.module.css";
+import Link from "next/link";
+import { TbShoppingCartX } from "react-icons/tb";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <Stepper step="cart" />
+
+      <main className={styles.cart} data-aos="fade-up">
+        <Table />
+      </main>
+
+      {/* <div class={styles.cart_empty} data-aos="fade-up">
+                <TbShoppingCartX />
+                <p>سبد خرید شما در حال حاضر خالی است. </p>
+                <span>قبل از تسویه حساب، باید چند محصول را به سبد خرید خود اضافه کنید.</span>
+                <span>در صفحه "فروشگاه"، محصولات جالب زیادی خواهید یافت.</span>
+                <div>
+                    <Link href='/category'>بازگشت به فروشگاه</Link>
+                </div>
+            </div> */}
+      <Footer />
+    </>
+  );
+};
+
+export default page;
