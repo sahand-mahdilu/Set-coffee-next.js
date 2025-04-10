da865c06c93a60a502c0fc29f4edab16eaf74083
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-10 14:50:04 +0330

    feat : product page added

---
+++ src/app/p-admin/products/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "@/components/templates/p-admin/products/table.module.css";
+import connectedToDB from "../../../../configs/db";
+import ProductModel from "../../../../models/Product";
+import AdminPanelLayout from "@/Components/layouts/AdminPanelLayout";
+import DataTable from "@/Components/templates/p-admin/products/Table";
+
+
+const page = async () => {
+ await connectedToDB()
+  const products = await ProductModel.find({}).sort({ _id: -1 }).lean();
+
+  return (
+    <AdminPanelLayout>
+      <main className=" pt-12">
+        {products.length === 0 ? (
+          <p className={styles.empty}>محصولی وجود ندارد</p>
+        ) : (
+          <DataTable
+            products={JSON.parse(JSON.stringify(products))}
+            title="لیست محصولات"
+          />
+        )}
+      </main>
+    </AdminPanelLayout>
+  );
+};
+
+export default page;
