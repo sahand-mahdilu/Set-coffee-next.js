31b84d30062e3eacb0d25cf27f7db995b596083b
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-04-12 12:56:28 +0330

    feat : type added to order component

--- src/Components/templates/p-user/Order.tsx
+++ src/Components/templates/p-user/Order.tsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 import styles from "./order.module.css";
+import { OrderType } from "@/app/types/types";
 
-const Order = ({name,price,count,img}) => {
+
+
+const Order: React.FC<OrderType> = ({ name, price, count, img }) => {
   return (
     <Link href={`/product/123`} className={styles.card}>
       <div>
         <div>
           <p className="max-sm:text-[12px]">{name}</p>
-          <img
-            src={img}
-            alt=""
-          />
+          <img src={img} alt="" />
         </div>
         <p className="max-sm:text-[12px]">درانتظار پرداخت</p>
-      </div> 
+      </div>
       <div>
         <p className="max-sm:text-[12px]">8:00 1402/10/21</p>
-        <p className={`${styles.price} className="max-sm:text-[12px]"` }>{price}</p>
+        <p className={`${styles.price} max-sm:text-[12px]`}>{price}</p>
         <p>تعداد : {count}</p>
       </div>
     </Link>
